afc5e055ea39c070531fa66356806afa186919bd
mynamavi <avinash.tripathi7@gmail.com>
2017-08-08 21:10:34 +0530

    added custom content for single only in cp-author post type.

--- includes/register-posts.php
+++ includes/register-posts.php
@@ -24,6 +24,7 @@ class CPR_AuthorReg
         add_action('save_post', array($this, 'cpr_save_author_data'));
         add_action('init', array($this, 'cpr_rewrite_rule'), 10, 0);
         add_action('init', array($this, 'cpr_rewrite_link'), 10, 0);
+        add_action('the_content', array($this, 'cpr_custom_content'), 10, 1);
         // Load frontend JS & CSS
         add_action('wp_enqueue_scripts', array($this, 'cp_register_styles'), 700);
         add_action('cpr_styles', array($this, 'cp_enqueue_styles'));
@@ -114,7 +115,8 @@ class CPR_AuthorReg
         }        
         return $title;
     }
-    public function cpr_add_styles_head(){
+    public function cpr_add_styles_head()
+    {
         $cpr_author_page='';
         $cpr_item_page='';
         $options = collectionpress_settings();
@@ -135,6 +137,84 @@ class CPR_AuthorReg
 
         if (is_singular('cp_authors')) {
             do_action("cpr_styles"); 
+        }
+    }
+
+    public function cpr_custom_content($content)
+    {
+        if (is_singular('cp_authors')) {
+            $post_content = $content;
+            ob_start();
+            ?>
+            <div class="author-items-wrap">
+                <?php
+                $show_items = get_post_meta(get_the_ID(), "show_items", true);
+                $author_keyword = get_post_meta(get_the_ID(), "author_keyword", true);
+                if ( $show_items=="yes" ){
+                    if ( $author_keyword=='' ){
+                        $author_keyword = get_the_title();
+                    }
+                    echo do_shortcode('[collectionpress author="'.$author_keyword.'"]');
+                }
+                ?>
+            </div>
+            <?php
+            $show_posts = get_post_meta(get_the_ID(), "show_posts", true);
+            $cp_related_author = get_post_meta(get_the_ID(), "cp_related_author", true);
+            if ($show_posts=="yes" && $cp_related_author!='') : ?>
+                <div class="author-posts-wrap">
+                    <h3><?php echo __("Author's Blog Posts", 'cpress') ?></h3>
+                    <?php
+                    $aposts=1;
+                    if (isset($_GET) && isset($_GET['aposts'])) {
+                        if ($_GET['aposts']!=''){
+                            $aposts = $_GET['aposts'];
+                        }
+                    }
+                    $author_posts = new WP_Query(array(
+                            "author" 	 	 =>$cp_related_author,
+                            "post_type"      =>"post",
+                            "post_status"    =>"publish",
+                            "orderby"        =>"modified",
+                            "order"          =>"DESC",
+                            "posts_per_page" =>get_option('posts_per_page'),
+                            "cache_results"  => false,
+                            "paged"          => $aposts));
+                    $found_posts =$author_posts->found_posts;
+                    $total_pages =$author_posts->max_num_pages;
+                    if ($author_posts->have_posts()) :
+                        while ($author_posts->have_posts()) :
+                            $author_posts->the_post();
+
+                            if (file_exists(locate_template('collectionpress/author_display_posts.php'))) {
+                                include(locate_template('collectionpress/author_display_posts.php'));
+                            } else {
+                                include(CPR_TEMPLATE_PATH.'/collectionpress/author_display_posts.php');
+                            }
+
+                        endwhile; ?>
+                        <div class="pagination">
+                        <?php
+                            $big = 999999999; // need an unlikely integer
+                            echo paginate_links(array(
+                                //~ 'base'      =>str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+                                'format'    =>'?aposts=%#%',
+                                'prev_text' =>__('&laquo;'),
+                                'next_text' =>__('&raquo;'),
+                                'current'   =>max(1, $aposts),
+                                'total'     =>$total_pages
+                                ));
+                            wp_reset_postdata();
+                        ?>
+                        </div>
+                    <?php else: ?>
+                        <p><?php echo __('No blog posts currently available.', 'cpress'); ?></p>
+                    <?php endif; ?>                
+                </div>
+            <?php
+            endif;
+            $post_content .= ob_get_clean();
+            return $post_content;
         }
     }
     
