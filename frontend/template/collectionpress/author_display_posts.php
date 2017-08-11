865bc82d6376c856cc6a5a94c28e38a534691bfc
mynamavi <avinash.tripathi7@gmail.com>
2017-08-11 12:53:21 +0530

    Following updates done: Template structure and calling method change. Name of template files also changed. Added Settings link in plugin directory. Added redirect to setting page after plugin activation.

--- frontend/template/collectionpress/author_display_posts.php
+++ frontend/template/collectionpress/author_display_posts.php
@@ -13,5 +13,5 @@
 } ?>
     </a>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php the_content(); ?>
-</article> <!-- .et_pb_post -->
+    <?php  the_excerpt(); ?>
+</article>
